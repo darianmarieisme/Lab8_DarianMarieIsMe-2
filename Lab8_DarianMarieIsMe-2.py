ff65a7986737f4a1b6394fce617383ab60ad74f4
Darian Bruce <darianmariebruce@gmail.com>
2026-03-03 21:41:08 -0500

    created menu

--- Lab8_DarianMarieIsMe-2.py
+++ Lab8_DarianMarieIsMe-2.py
@@ -3,3 +3,25 @@ Darian Marie Bruce
 This program calculates the area of perimeter of circles
 and rectangles
 03/03/2026'''
+
+menu: bool = True
+
+#color codes ansi escape sequences
+
+orange: str = "\033[38;5;208m"
+purple: str = "\033[38;5;129m"
+reset: str = "\033[0m"
+
+while menu:
+    print("Geometry Calculator")
+    print("-"*19)
+    print(
+    f"\n{orange}1.{reset} Calculate Circle Area",
+    f"\n{orange}2.{reset} Calculate Circle Circumference",
+    f"\n{orange}3.{reset} Calculate Rectangle Area",
+    f"\n{orange}4.{reset} Calculate Rectangle Perimeter",
+    f"\n{orange}5.{reset} Exit\n \n ")
+
+    menu_option: int = int(input(f"\nEnter your choice ({orange}1-5{reset}): "))
+
+    break
