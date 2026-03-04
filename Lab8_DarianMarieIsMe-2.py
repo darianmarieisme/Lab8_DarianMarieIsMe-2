4a0077025802d9d97e308c0adb117d8d57ff086c
Darian Bruce <darianmariebruce@gmail.com>
2026-03-03 22:22:10 -0500

    validating menu input

--- Lab8_DarianMarieIsMe-2.py
+++ Lab8_DarianMarieIsMe-2.py
@@ -5,6 +5,8 @@ and rectangles
 03/03/2026'''
 
 menu: bool = True
+invalid_menu_input: bool = True
+invalid_input: str = "Invalid input. Please try again."
 
 #color codes ansi escape sequences
 
@@ -20,8 +22,18 @@ while menu:
     f"\n{orange}2.{reset} Calculate Circle Circumference",
     f"\n{orange}3.{reset} Calculate Rectangle Area",
     f"\n{orange}4.{reset} Calculate Rectangle Perimeter",
-    f"\n{orange}5.{reset} Exit\n \n ")
+    f"\n{orange}5.{reset} Exit\n ")
 
-    menu_option: int = int(input(f"\nEnter your choice ({orange}1-5{reset}): "))
+    while invalid_menu_input:
+            user_input: str = input(f"\nEnter your choice ({orange}1-5{reset}): ")
+            
+            if user_input.isdigit():
+                  menu_option: int = int(user_input)
+
+            if 1 <= menu_option <= 5:
+                 invalid_menu_input = False
+
+            else:
+                 print(invalid_input)
 
     break
---
+++ circle.py
@@ -0,0 +1,3 @@
+def get_radius()
+    
+def get_circumference()
